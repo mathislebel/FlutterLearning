f364c29020c692bbf92fff34a30f087f196f19ba
Mathis LEBEL <145991354+mathislebel@users.noreply.github.com>
2024-05-31 10:58:31 +0200

    I inisialize the main and the welcome page

---
+++ dating_app/lib/WelcomePage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xff101010),
+      ),
+    );
+  }
+}
