000fbd400575a40e4cac43bfb607790fdb7f848a
Mathis LEBEL <145991354+mathislebel@users.noreply.github.com>
2024-05-31 11:29:05 +0200

    hh

--- dating_app/lib/main.dart
+++ dating_app/lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:dating_app/WelcomePage.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(DatingApp());
-}
 
 class DatingApp extends StatelessWidget {
-  const DatingApp({super.key});
 
   @override
   Widget build(BuildContext context) {
